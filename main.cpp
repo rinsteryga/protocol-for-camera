d767caf6305e1161f434c2c0ec9ababd3e30f41e
Sernock <shprotokot@gmail.com>
2026-03-16 12:54:46 +0300

    build: update project structure and CMake configurations

--- main.cpp
+++ main.cpp
@@ -1,21 +1,14 @@
+#include <QCoreApplication>
 #include "VideoSender.hpp"
-
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
-#include <QQmlContext>
 
 int main(int argc, char *argv[])
 {
-    QGuiApplication app(argc, argv);
+    // Используем QCoreApplication для работы без GUI
+    QCoreApplication app(argc, argv);
+
     VideoSender sender;
-    QQmlApplicationEngine engine;
-
-    engine.rootContext()->setContextProperty("sender", &sender);
-
-    QObject::connect(
-        &engine, &QQmlApplicationEngine::objectCreationFailed, &app, []() { QCoreApplication::exit(-1); },
-        Qt::QueuedConnection);
-    engine.loadFromModule("VideoSender", "Main");
+    // Можно добавить вывод в консоль, что сервер запущен
+    qInfo() << "Video Server is running. Press Ctrl+C to stop.";
 
     return app.exec();
 }
